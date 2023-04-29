9f6bcc2e113a298bf64c051e8fae27a7ff04aaa6
jackdaniel2008 <126785454+jackdaniel2008@users.noreply.github.com>
2023-04-29 13:00:07 +0000

    管理者側の商品編集画面作成まで

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,11 +21,21 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price)
+    params.require(:item).permit(:name, :introduction, :price, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :history_details, dependent: :destroy
 
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
 end
