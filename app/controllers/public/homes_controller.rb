3b753005dc9b4ed94b8fe863ed4106e258d8d039
jackdaniel2008 <126785454+jackdaniel2008@users.noreply.github.com>
2023-05-19 14:59:51 +0000

    商品一覧のレイアウトから

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
     # :asc,古い順 :desc,新しい順
-    @items = Item.all.order(created_at: :asc)
+    @items = Item.all.order(created_at: :desc)
   end
-  
+
   def about
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   
   def index
+    @items_all = Item.all
     @items = Item.page(params[:page]).per(8)
   end
 
